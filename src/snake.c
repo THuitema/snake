b72a0527e700f146ea0cab0f04fc882ca6261a03
Thomas Huitema <thuitema35@gmail.com>
2024-07-24 00:29:14 -0400

    Begin drawing snake block on screen and control direction with arrows

--- src/snake.c
+++ src/snake.c
@@ -7,26 +7,63 @@
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 600
 #define FPS 30
+#define SNAKE_WIDTH 50
+
+static Snake *init_snake(void);
+static void update_snake(Snake *snake);
 
 int main(void) {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
-    SetTargetFPS(60);
+    SetTargetFPS(FPS);
 
-    Snake *c = malloc(sizeof(Snake));
+    Snake *snake = init_snake();
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
-            DrawText("Start Snake!", 190, 200, 20, DARKGRAY);
+            draw_snake(snake);
         EndDrawing();
-
-        if (IsKeyDown(KEY_LEFT)) printf("Left\n");
-        if (IsKeyDown(KEY_RIGHT)) printf("Right\n");
-        if (IsKeyDown(KEY_UP)) printf("Up\n");
-        if (IsKeyDown(KEY_DOWN)) printf("Down\n");        
+        
+        update_snake(snake);
     }
 
     CloseWindow();
 
     return 0;
 }
+
+static Snake *init_snake() {
+    Snake *snake = malloc(sizeof(Snake));
+    snake->size = 1;
+    Vector2 shape = {SNAKE_WIDTH, SNAKE_WIDTH};
+    snake->shape = shape;
+
+    /* Initialize head of snake */
+    snake->head = malloc(sizeof(SnakeCell));
+    snake->head->direction = RIGHT;
+
+    Vector2 position = {0, 0};
+    snake->head->position = position;
+
+    return snake;
+}
+
+static void update_snake(Snake *snake) {
+    if (IsKeyDown(KEY_LEFT)) snake->head->direction = LEFT;
+    if (IsKeyDown(KEY_RIGHT)) snake->head->direction = RIGHT;
+    if (IsKeyDown(KEY_UP)) snake->head->direction = UP;
+    if (IsKeyDown(KEY_DOWN)) snake->head->direction = DOWN; 
+
+    /* See if we need to update direction */
+    if (snake->head->direction == LEFT) snake->head->position.x -= SNAKE_WIDTH;
+    if (snake->head->direction == RIGHT) snake->head->position.x += SNAKE_WIDTH;
+    if (snake->head->direction == UP) snake->head->position.y -= SNAKE_WIDTH;
+    if (snake->head->direction == DOWN) snake->head->position.y += SNAKE_WIDTH;
+}
+
+void draw_snake(Snake *snake) {
+    /* Will have to implement some kind of loop to draw each SnakeCell and update positions */
+    DrawRectangleV(snake->head->position, snake->shape, GREEN);
+}
+
+
--- src/snake.h
+++ src/snake.h
@@ -1,7 +1,20 @@
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction;
+
 typedef struct SnakeCell {
+    Vector2 position;
+    Direction direction;
     struct SnakeCell *next;
 } SnakeCell;
 
 typedef struct {
+    int size;
+    Vector2 shape;
     SnakeCell *head;
 } Snake;
+
+void draw_snake(Snake *snake);
